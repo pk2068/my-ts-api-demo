8c64556092e00a6d4bf0b56ef235441e4253dea5
pk2068 <pkorosec@gmail.com>
2025-06-27 10:42:44 +0200

    refactor: Remove server.ts and integrate its functionality into appExpressInstance.ts

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -3,12 +3,7 @@
 import MyCustomError from '@src/types/errors.js';
 import { Request, Response, NextFunction } from 'express';
 
-// Define a custom error type for better clarity (optional)
-interface CustomError extends Error {
-  statusCode?: number;
-  data?: string | string[] | object; // Additional data to include in the error response
-  stack?: string | undefined; // Optional stack trace for debugging
-}
+
 
 // Global error handling middleware
 const errorHandler = (err: MyCustomError, req: Request, res: Response, _next: NextFunction) => {
